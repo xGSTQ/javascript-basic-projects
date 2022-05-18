4d697971bbe4bd85069603de4f732df899f2d2b3
James Pedlar <james.pedlar@jellyfish.com>
2022-05-18 15:58:25 +0100

    01 | Finished

--- 01-color-flipper/mine/app.js
+++ 01-color-flipper/mine/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// Get random number
+let getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+// Simple Background Colour Update
+btn.addEventListener('click', () => {
+    const mainElement = document.getElementsByTagName('main')[0];
+    const randomNumber = getRandomInt(colors.length);
+    mainElement.style.backgroundColor = colors[randomNumber];
+    color.innerHTML = colors[randomNumber];
+});
--- 01-color-flipper/mine/hex.js
+++ 01-color-flipper/mine/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// Get random hex colour code
+let generateRandomColor = () => {
+    let hexColor = [];
+
+    for (let i=0; i < 6; i++) {
+        let random = hex[Math.floor(Math.random() * 16)];
+        hexColor.push(random);
+    }
+    return '#' + hexColor.join('');
+}
+
+// Hex background colour update
+btn.addEventListener('click', () => {
+    const mainElement = document.getElementsByTagName('main')[0];
+    const randomColor = generateRandomColor();
+    mainElement.style.backgroundColor = randomColor;
+    color.innerHTML = randomColor;
+});
