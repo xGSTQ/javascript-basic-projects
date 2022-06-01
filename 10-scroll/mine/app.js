a102f07cb5aeb3761422acf4397af1aa7c7ed481
James <xgodsavethequeen@gmail.com>
2022-06-01 16:43:27 +0100

    10 | WIP

---
+++ 10-scroll/mine/app.js
@@ -0,0 +1,19 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+const dateEl = document.getElementById('date').innerHTML = new Date().getFullYear();
+
+// ********** mobile nav links ************
+const navToggleEl = document.querySelector('.nav-toggle');
+const linksContainerEl = document.querySelector('.links-container');
+const linksEl = document.querySelector('.links');
+
+
+
+// ********** fixed navbar ************
+
+// ********** smooth scroll ************
+// select links
