ede98610b6c57afd2571e2c323067b17cec6b092
James Pedlar <james.pedlar@jellyfish.com>
2022-09-09 09:01:18 +0100

    14 | WIP

--- 14-grocery-bud/mine/app.js
+++ 14-grocery-bud/mine/app.js
@@ -18,47 +18,47 @@ let editID = '';
 // ****** FUNCTIONS **********
 // submit form function
 addItem = (e) => {
-    e.preventDefault();
-    const value = grocery.value;
-    const id = new Date().getTime().toString();
-    // form logic
-    if (value && !editFlag) {
-        console.log('add to list');
-        
-        const element = document.createElement('article');
-        element.classList.add('grocery-item');
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  // form logic
+  if (value && !editFlag) {
+    console.log('added to list');
 
-        element.innerHTML = `<p class="title">${value}</p>
+    const element = document.createElement('article');
+    element.classList.add('grocery-item');
+
+    element.innerHTML = `<p class="title">${value}</p>
                             <div class="btn-container">
-                            <button type="button" class="edit-btn">
-                                <i class="fas fa-edit"></i>
-                            </button>
-                            <button type="button" class="delete-btn">
-                                <i class="fas fa-trash"></i>
-                            </button>
+                              <button type="button" class="edit-btn">
+                                  <i class="fas fa-edit"></i>
+                              </button>
+                              <button type="button" class="delete-btn">
+                                  <i class="fas fa-trash"></i>
+                              </button>
                             </div>`;
 
-        list.appendChild(element);
-        container.classList.add('show-container');
-        
+    list.appendChild(element);
+    container.classList.add('show-container');
 
-    } else if (value && editFlag) {
-        console.log('editing');
-    } else {
-        // console.log('empty value');
-        displayAlert('please enter value', 'danger')
-    }
+
+  } else if (value && editFlag) {
+    console.log('editing');
+  } else {
+    // console.log('empty value');
+    displayAlert('please enter value', 'danger')
+  }
 }
 
 //display the alert function
 displayAlert = (text, action) => {
-    alert.textContent = text;
-    alert.classList.add(`alert-${action}`);
-    // remove alert
-    setTimeout(() => {
-        alert.textContent = '';
-        alert.classList.remove(`alert-${action}`);
-    }, 1000);
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  // remove alert
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
 }
 
 
